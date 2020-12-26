16ee8414a2ca88b852c7fb0d13353c49371369c8
username <finnyebby@gmail.com>
2020-12-27 00:40:16 +0530

    Added basic home page and login form validation

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -10,7 +10,6 @@ import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
-
 class Login extends Component {
 
     constructor() {
@@ -19,23 +18,32 @@ class Login extends Component {
             username: "",
             password: "",
             usernameRequired: "dispNone",
-            passwordRequired: "dispNone"
+            passwordRequired: "dispNone",
+            userValidated: "dispNone"
         }
     }
 
     usernameChangeHandler = (e) => {
-        this.setState({username: e.target.value})
+        this.setState({ username: e.target.value })
     }
 
     passwordChangeHandler = (e) => {
-        this.setState({password: e.target.value})
+        this.setState({ password: e.target.value })
     }
 
     loginHandler = () => {
-        this.state.username == "" ? this.setState({usernameRequired: "dispBlock"}) : this.setState({usernameRequired: "dispNone"})
-        this.state.password == "" ? this.setState({passwordRequired: "dispBlock"}) : this.setState({passwordRequired: "dispNone"})
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" })
+        this.state.password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" })
+        let loginUsername = "user123"
+        let loginPassword = "passcode"
+        this.state.username && this.state.password ? 
+        (this.state.username !== loginUsername || this.state.password !== loginPassword ? this.setState({userValidated: "dispBlock" }) : this.setState({ userValidated: "dispNone" })) : 
+        this.setState({ userValidated: "dispNone" })
     }
 
+    execLater = () => {
+        console.log(this.state.passwordRequired)
+    }
     render() {
         return (
             <div>
@@ -43,7 +51,7 @@ class Login extends Component {
                 <div>
                     <Card className="cardStyle">
                         <CardContent>
-                            <Typography className="unselectable" variant="headline" component="h2">LOGIN</Typography><br />
+                            <Typography className="unselectable" variant="h5">LOGIN</Typography><br />
                             <FormControl required className="formControl">
                                 <InputLabel className="unselectable" htmlFor="username">Username</InputLabel>
                                 <Input id="username" type="text" onChange={this.usernameChangeHandler} />
@@ -58,6 +66,9 @@ class Login extends Component {
                                     <span className="red unselectable">required</span>
                                 </FormHelperText>
                             </FormControl><br /><br />
+                            <FormHelperText className={this.state.userValidated}>
+                                <span className="red unselectable">Incorrect username and/or password</span>
+                            </FormHelperText><br />
                             <Button variant="contained" color="primary" onClick={this.loginHandler}>LOGIN</Button>
                         </CardContent>
                     </Card>
