5f417b70d824be9e785646cb269cd0674b92998e
username <finnyebby@gmail.com>
2020-12-26 15:24:42 +0530

    had to update package.json due to some dependency errors

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './Login.css';
 import Header from '../../common/header/Header';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
 
 class Login extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <div>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="headline" component="h2">LOGIN</Typography>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         )
     }
