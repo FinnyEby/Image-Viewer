679df3062935ded9b85dc9a3d5ccf0ea7442e4ac
username <finnyebby@gmail.com>
2020-12-26 15:47:23 +0530

    Added Login Form

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -5,6 +5,10 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+
 
 class Login extends Component {
     render() {
@@ -12,9 +16,18 @@ class Login extends Component {
             <div>
                 <Header />
                 <div>
-                    <Card>
+                    <Card className="cardStyle">
                         <CardContent>
-                            <Typography variant="headline" component="h2">LOGIN</Typography>
+                            <Typography variant="headline" component="h2">LOGIN</Typography><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="username">Username</InputLabel>
+                                <Input id="username" type="text"/>
+                            </FormControl><br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input id="password" type="password"/>
+                            </FormControl><br /><br />
+                            <Button variant="contained" color="primary">LOGIN</Button>
                         </CardContent>
                     </Card>
                 </div>
