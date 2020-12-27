0221f220ca9bb419184138f745eab4634e76e731
username <finnyebby@gmail.com>
2020-12-27 12:44:19 +0530

    Added Searchbar and Iconbutton to header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,48 @@
 import './Header.css';
 import React, { Component } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+import img from '../../assets/finn.png'
+import Avatar from '@material-ui/core/Avatar';
 
 class Header extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            showMenu: 0
+        }
+    }
+
+    iconButtonClickHandler = () => {
+        this.setState({ showMenu: 1 })
+    }
+
     render() {
         return (
             <div className="header unselectable">
-                <div className="logo">
-                    Image Viewer
+                <div className="innerHeader">
+                    <div className="logo">
+                        Image Viewer
+                </div>
+                    {this.props.dispalyUserOptions === true &&
+                        <div className="loggedInUserOptions">
+                            <div className="search">
+                                <div className="searchIcon">
+                                    <SearchIcon />
+                                </div>
+                                <Input disableUnderline={true} placeholder="Search…" />
+                            </div>
+                            <div>
+                                <IconButton size="small" onClick={this.iconButtonClickHandler}>
+                                    <Avatar src={img}></Avatar>
+                                </IconButton>
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         )
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './Home.css';
-import Header from '../../common/header/Header'
+import Header from '../../common/header/Header';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <Header />
+                <Header dispalyUserOptions={true} />
                 <div className="home">Home Page</div>
             </div>
         )
